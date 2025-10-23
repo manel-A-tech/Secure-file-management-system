9e0443d1f17b0bee7360b9236f29c633c5ebc11b
manel-A-tech <manelameziane215@gmail.com>
2025-10-23 12:22:14 +0100

    registration and login forms

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask , render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/loging")
 def index():
-  return "Test"
+  return render_template("loging.html")
 
 
-if __name__ in "__main__":
+if __name__ == "__main__":
   app.run(debug=True)
